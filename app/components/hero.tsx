7344688d9792f6f5c68fcc57ef6bf23e734218a1
Ankit Kumar Jat <ankit.kumar@affle.com>
2024-09-20 19:52:23 +0530

    feat: hero section added

---
+++ app/components/hero.tsx
@@ -0,0 +1,35 @@
+import { Form } from '@remix-run/react'
+import { SearchIcon } from 'lucide-react'
+
+function Hero() {
+  return (
+    <div className="container py-14 text-center">
+      <h1 className="mx-auto max-w-5xl text-balance text-3xl sm:text-5xl lg:text-6xl">
+        Discover Millions of Books at Your Fingertips
+      </h1>
+      <p className="mx-auto mt-6 max-w-screen-sm text-balance text-sm md:text-base lg:max-w-screen-md">
+        Explore a vast library of books across genres and authors. With Novalla,
+        finding your next great read is just a search away. Dive into a world of
+        literature, from timeless classics to contemporary gems.
+      </p>
+
+      <Form
+        action="/search"
+        method="get"
+        className="mx-auto mt-6 flex max-w-lg rounded-full border"
+      >
+        <input
+          name="q"
+          className="h-12 w-full rounded-s-full pl-6"
+          placeholder="Search Book title, author..."
+        />
+        <button className="rounded-e-full border-l p-3 pr-4">
+          <span className="sr-only">Search</span>
+          <SearchIcon />
+        </button>
+      </Form>
+    </div>
+  )
+}
+
+export default Hero
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,21 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
+import Hero from '~/components/hero'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Novalla - Search Millions of Books Instantly" },
+    { title: 'Novalla - Search Millions of Books Instantly' },
     {
-      name: "description",
+      name: 'description',
       content:
         "Explore Novalla's vast collection of books across all genres. Search, discover, and dive into millions of books with ease. Find your next favorite read on Novalla!",
     },
-  ];
-};
+  ]
+}
 
 export default function Index() {
-  return <div className="  r">Hello World!</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  )
 }
