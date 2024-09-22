23a900186fb230ab67795a02cb5441fe290e3ce6
Ankit Kumar Jat <ankit.kumar@affle.com>
2024-09-22 15:34:13 +0530

    feat: added utility functions to get cover/author image url

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export interface CoverImageOptions {
+  type: 'isbn' | 'oclc' | 'olid' | 'id'
+  id: string | number
+  size?: 'S' | 'M' | 'L'
+}
+
+export function getCoverImage({ type, id, size = 'M' }: CoverImageOptions) {
+  return `https://covers.openlibrary.org/b/${type}/${id}-${size}.jpg`
+}
+
+export interface AuthorImageOptions {
+  type: 'olid' | 'id'
+  id: string | number
+  size?: 'S' | 'M' | 'L'
+}
+
+export function getAuthorImage(options: AuthorImageOptions) {
+  return getCoverImage(options)
 }
