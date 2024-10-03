31416fb965a5905fc14b8651bc2b96d5aed276d5
Ankit Kumar Jat <ankit.kumar@affle.com>
2024-10-03 12:33:43 +0530

    feat: author details page

--- app/lib/api/authors.server.ts
+++ app/lib/api/authors.server.ts
@@ -2,7 +2,7 @@ import { Author, ListResponse, Work } from '../api-types'
 import { openLibApiClient, WEEKLY_CACHE_OPTIONS } from './api-client.server'
 
 export async function getAuthorById({ authorId }: { authorId: string }) {
-  const author = await openLibApiClient<Author>(`/author/${authorId}.json`, {
+  const author = await openLibApiClient<Author>(`/authors/${authorId}.json`, {
     cf: WEEKLY_CACHE_OPTIONS,
   })
 
@@ -17,10 +17,12 @@ interface GetBooksByAuthorIdOptions {
 
 export async function getWorksByAuthorId({
   authorId,
+  limit = 20,
+  offset = 0,
 }: GetBooksByAuthorIdOptions) {
   const authorWorks = await openLibApiClient<ListResponse<Work>>(
-    `/author/${authorId}/works.json`,
-    { cf: WEEKLY_CACHE_OPTIONS },
+    `/authors/${authorId}/works.json`,
+    { params: { limit, offset }, cf: WEEKLY_CACHE_OPTIONS },
   )
 
   return authorWorks
