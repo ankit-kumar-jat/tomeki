cfa5df215c2c5465911ce5dd38e90faba0eafbe4
Ankit Kumar Jat <ankit.kumar@affle.com>
2024-10-17 17:54:43 +0530

    fix: favicon changes to look good in google search

--- app/root.tsx
+++ app/root.tsx
@@ -57,26 +57,21 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta name="google-adsense-account" content="ca-pub-6892126566030270" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-48x48.png"
+          sizes="48x48"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#" />
+        <meta name="apple-mobile-web-app-title" content="Tomeki" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta
           name="theme-color"
           content={theme === 'dark' ? '#000000' : '#ffffff'}
